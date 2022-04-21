917303ecff9fe89163cc9af859273a26f74c53db
Tales Ribeiro <taalesr@gmail.com>
2022-04-20 22:35:42 -0300

    Bot

--- notification-app/app.js
+++ notification-app/app.js
@@ -1,10 +1,17 @@
 const { Kafka } = require('kafkajs')
+const TelegramBot = require('node-telegram-bot-api');
 
 const TOPIC = 'notify'
+const token = '5316988190:AAHMLldZf6GafRq_TXpQ22gddmDQnjE6b3w';
+let action = ''
 
 const kafka = new Kafka({
   clientId: 'client-1',
-  brokers: ['kafka1:19091', 'kafka2:19092', 'kafka3:19093']
+  brokers: ['localhost:19091', 'localhost:19092', 'localhost:19093'],
+  retry: {
+    initialRetryTime: 300,
+    retries: 10
+  },
 })
 
 const consumer = kafka.consumer({ groupId: 'notify-group' })
@@ -12,17 +19,29 @@ const consumer = kafka.consumer({ groupId: 'notify-group' })
 const run = async () => {
   // Consuming
   await consumer.connect()
+  
   await consumer.subscribe({ topic: TOPIC, fromBeginning: true })
-
+  console.log('conectou')
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        partition,
-        offset: message.offset,
-        value: message.value.toString(),
-      })
+      console.log('sim')
+      const bot = new TelegramBot(token, {polling: true});
+
+      if (message.action == 'rotate') {
+        let action = 'rotacionado'
+      } else {
+        let action = 'transformado em preto e branco'
+      }
+
+      const text = `O arquivo ${message.new_file} foi ${action}`
+      bot.sendMessage(957366775, text)
     },
   })
 }
 
 run().catch(console.error)
+
+
+
+
+
--- notification-app/bot-telegram.js
+++
@@ -1,7 +0,0 @@
-const TelegramBot = require('node-telegram-bot-api');
-const token = '5316988190:AAHMLldZf6GafRq_TXpQ22gddmDQnjE6b3w';
-const bot = new TelegramBot(token, {polling: true});
-const text = "Tudo funcionando"
-
-
-bot.sendMessage(957366775, text)
