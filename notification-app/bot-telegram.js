7be60c8d82fe1b183c8afafe20c55fdfdf2f17cf
Marcos Barros <marcosa439@gmail.com>
2022-04-20 21:04:09 -0300

    Troca a mensagem padrão do bot

--- notification-app/bot-telegram.js
+++ notification-app/bot-telegram.js
@@ -1,7 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = '5316988190:AAHMLldZf6GafRq_TXpQ22gddmDQnjE6b3w';
 const bot = new TelegramBot(token, {polling: true});
-const text = "Tudo funcionando"
+const text = "O arquivo foi rotacionado e foi transformado em escala de cinza"
 
 
 bot.sendMessage(957366775, text)
